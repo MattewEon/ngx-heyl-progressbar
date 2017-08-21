6e1ace99f2dbf0eef8fd1471ff02f9d40e13d189
MattewEon <mattewphoenyx@hotmail.fr>
2017-08-21 09:21:29 +0200

    Css fix
    fixed stripped-reverse not working

--- index.ts
+++ index.ts
@@ -2,12 +2,14 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {ProgressbarComponent, ProgressContainerComponent} from "./lib/components/progressbar.component";
 import {RadialProgressComponent} from "./lib/components/radial-progress.component";
+import {AbstractProgressComponent} from "./lib/components/abstract-progress.component";
 
 @NgModule({
 	imports: [
 		CommonModule
 	],
 	declarations: [
+		AbstractProgressComponent,
 		ProgressContainerComponent,
 		ProgressbarComponent,
 		RadialProgressComponent
